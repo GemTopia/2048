bec2ac2869801031b5b3cfe09d38273a8c8dff98
meti <mahdi.mazloomi03@gmail.com>
2023-07-25 23:30:27 +0400

    game

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -128,7 +128,7 @@ HTMLActuator.prototype.message = function (won) {
   var type = won ? "game-won" : "game-over";
   var message = won ? "You win!" : "Game over!";
   console.log(localStorage.getItem("bestScore"));
-  document.location.replace(`http://gem.kveh.ir/result/3/${Number(highScoreStr)*17-68}`)
+  document.location.replace(`http://gem.kveh.ir/result/3/${Number(localStorage.getItem("bestScore"))*17-68}`)
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
@@ -139,3 +139,6 @@ HTMLActuator.prototype.clearMessage = function () {
   this.messageContainer.classList.remove("game-won");
   this.messageContainer.classList.remove("game-over");
 };
+window.onload = function() {
+  localStorage.removeItem("bestScore")
+}
